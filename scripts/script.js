30ef0619917f16de2e106f915975c7f3e06364f2
jimin947 <jimin947@gmail.com>
2021-02-14 16:12:46 +0900

    Finish

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,91 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  let myStorage = window.localStorage;
+
+  let cart = 0;
+  if(myStorage.getItem('cart')!=null){
+    cart = myStorage.getItem('cart');
+    document.getElementById("cart-count").innerHTML = cart;
+  }
+
+  if(myStorage.getItem("list") == null) {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      myStorage.setItem('list', JSON.stringify(data));
+      const loop = JSON.parse(myStorage.getItem('list'));
+      for(let i = 0; i < loop.length; i++) {
+        let product = document.createElement('product-item');
+        product.className = 'product';
+        var shad = product.shadowRoot;
+
+        shad.querySelector('.title').textContent = loop[i].title;
+        shad.querySelector('.price').textContent = '$' + loop[i].price;
+        shad.querySelector('img').src = loop[i].image;
+        shad.querySelector('img').alt = loop[i].title;
+        shad.querySelector('button').id = i;
+
+        shad.querySelector('button').setAttribute('add',false);
+
+        if(localStorage.getItem(i)) {
+          shad.querySelector('button').setAttribute('add', true);
+          shad.querySelector('button').textContent = 'Remove From Cart';
+        }
+
+        shad.querySelector('button').onclick = function(){ 
+          add(this)
+        }
+        document.getElementById('product-list').appendChild(item);
+      }
+    });
+  } else {
+    const loop = JSON.parse(myStorage.getItem('list'));
+      for(let i = 0; i < loop.length; i++) {
+        let product = document.createElement('product-item');
+        var shad = product.shadowRoot;
+
+        shad.querySelector('.title').textContent = loop[i].title;
+        shad.querySelector('.price').textContent = '$' + loop[i].price;
+        shad.querySelector('img').src = loop[i].image;
+        shad.querySelector('img').alt = loop[i].title;
+        shad.querySelector('button').id = i;
+
+        shad.querySelector('button').setAttribute('add',false);
+
+        if(localStorage.getItem(i)) {
+          shad.querySelector('button').setAttribute('add', true);
+          shad.querySelector('button').textContent = 'Remove From Cart';
+        }
+
+        shad.querySelector('button').onclick = function(){ 
+          add(this) 
+        }
+        document.getElementById('product-list').appendChild(product);
+      }
+  }
+  
+  function add(e) {
+    if(e.getAttribute('add')== 'false'){
+      alert ('Added to Cart!');
+      myStorage.setItem('cart',cart);
+      cart = cart + 1;
+      document.getElementById('cart-count').innerHTML = cart;
+      
+      e.textContent = 'Remove from Cart';
+      e.setAttribute('add',true);
+      myStorage.setItem(e.id,'added');
+    }
+    else{
+      alert ('Removed from Cart!');
+      e.textContent = 'Add to Cart';
+      myStorage.setItem('cart',cart);
+      cart = cart - 1;
+      document.getElementById('cart-count').innerHTML = cart;
+      
+      myStorage.removeItem(e.id);
+      e.setAttribute('add',false);
+    }
+  }
 });
+
